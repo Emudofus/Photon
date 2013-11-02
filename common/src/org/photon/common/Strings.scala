e6c457b2354b15ae4d201903eb002d6ba2a82444
Blackrush <blackrushx@gmail.com>
2013-11-02 18:22:29 +0100

    correctly checks password validity

--- common/src/org/photon/common/Strings.scala
+++ common/src/org/photon/common/Strings.scala
@@ -6,4 +6,32 @@ object Strings {
   val alphanum = 'a' to 'z'
   def rand(implicit rnd: Random): Char = alphanum(rnd.nextInt(alphanum.length))
   def next(n: Int)(implicit rnd: Random): String = (1 to n).foldLeft("") { (res, _) => res + rand }
+
+
+  def decryptDofusPassword(input: String, key: String): String = {
+    val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
+    val len = alphabet.length
+
+    val decrypted = StringBuilder.newBuilder
+    decrypted.sizeHint(input.length / 2)
+
+    for (i <- Range(0, input.length, step = 2)) {
+      val PKey = key(i / 2)
+
+      val ANB = alphabet indexOf input(i)
+      val ANB2 = alphabet indexOf input(i + 1)
+
+      var APass = ANB + len - PKey
+      if (APass < 0) APass += 64
+      APass <<= 4
+
+      var AKey = ANB2 + len - PKey
+      if (AKey < 0) AKey += 64
+
+      val PPass = (APass + AKey).toChar
+      decrypted += PPass
+    }
+
+    decrypted.result
+  }
 }
--- login/src/org/photon/login/UserAuthenticationComponentImpl.scala
+++ login/src/org/photon/login/UserAuthenticationComponentImpl.scala
@@ -1,15 +1,19 @@
 package org.photon.login
 
 import com.twitter.util.{Future, Throw, Return}
+import org.photon.common.Strings
 
 trait UserAuthenticationComponentImpl extends UserAuthenticationComponent {
   self: UserRepositoryComponent =>
 
   import Future.{exception => throws}
+  import Strings.{decryptDofusPassword => decrypt}
+
+  def validPassword(user: User, password: String, key: String) = user.password == decrypt(password, key)
 
   def authenticate(s: NetworkSession, username: String, password: String) = users.find(username).transform {
     case Return(user) =>
-      if (user.password != password)
+      if (!validPassword(user, password, s.ticket))
         throws(AccessDeniedException())
 
       // TODO check if user is banned, already connected, ...
