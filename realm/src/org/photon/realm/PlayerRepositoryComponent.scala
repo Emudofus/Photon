d08d76d225f5b7f6db55a2303745eda6e35ac37d
Blackrush <blackrushx@gmail.com>
2013-11-24 00:15:05 +0100

    adds some columns

--- realm/src/org/photon/realm/PlayerRepositoryComponent.scala
+++ realm/src/org/photon/realm/PlayerRepositoryComponent.scala
@@ -5,15 +5,37 @@ import org.photon.common.persist.{Repository, Model, ModelState}
 import org.photon.common.persist.ModelState.ModelState
 import org.photon.protocol.dofus.account.{Player => PlayerTemplate}
 
+case class Colors(first: Int, second: Int, third: Int)
+
+class PlayerAppearence(val skin: Short, val colors: Colors) {
+  def accessories: Seq[Int] = Seq.fill(5)(0)
+}
+
 case class Player(
   id: Long,
   ownerId: Long,
   name: String,
+  level: Short,
+  appearence: PlayerAppearence,
   state: ModelState = ModelState.None
 ) extends Model {
   type PrimaryKey = Long
 
-  def toPlayerTemplate: PlayerTemplate = ???
+  def toPlayerTemplate = PlayerTemplate(
+    id,
+    name,
+    level,
+    appearence.skin,
+    appearence.colors.first,
+    appearence.colors.second,
+    appearence.colors.third,
+    appearence.accessories,
+    merchant = false,
+    serverId = -1,
+    dead = false,
+    deathCount = 0,
+    levelMax = 200
+  )
 }
 
 trait PlayerRepository extends Repository[Player] {
--- realm/src/org/photon/realm/PlayerRepositoryComponentImpl.scala
+++ realm/src/org/photon/realm/PlayerRepositoryComponentImpl.scala
@@ -14,18 +14,33 @@ trait PlayerRepositoryComponentImpl extends PlayerRepositoryComponent {
   class PlayerRepositoryImpl extends BaseRepository[Player](self.database) with PlayerRepository {
     lazy val table = "players"
     lazy val pkColumns = Seq("id")
-    lazy val columns = Seq("owner_id", "name")
+    lazy val columns = Seq("owner_id", "name", "level", "skin", "color1", "color2", "color3")
 
     def buildModel(rset: ResultSet) = Player(
       rset.getLong("id"),
       rset.getLong("owner_id"),
-      rset.getString("name")
+      rset.getString("name"),
+      rset.getShort("level"),
+      new PlayerAppearence(
+        rset.getShort("skin"),
+        Colors(
+          rset.getInt("color1"),
+          rset.getInt("color2"),
+          rset.getInt("color3")
+        )
+      ),
+      ModelState.Persisted
     )
 
     def bindParams(ps: PreparedStatement, user: Player) {
       ps.set(1, user.id)
       ps.set(2, user.ownerId)
       ps.set(3, user.name)
+      ps.set(4, user.level)
+      ps.set(5, user.appearence.skin)
+      ps.set(6, user.appearence.colors.first)
+      ps.set(7, user.appearence.colors.second)
+      ps.set(8, user.appearence.colors.third)
     }
 
     def setPersisted(o: Player, newId: Long) = o.copy(id = newId, state = ModelState.Persisted)
